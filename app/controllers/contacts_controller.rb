e319acb25af13b3edb8732752344cf1e7d45ae28
Harshit Shetty <swaharsh@gmail.com>
2021-09-24 00:34:23 +0530

    add create convo api

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,5 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all 
     render json: @contacts
-end 
+  end 
 end
--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,20 @@
 class ConversationsController < ApplicationController
+  def index
+    @conversations = Conversation.last(10)
+    render json: @conversations
+  end 
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+    if @conversation.save
+      render json: @conversation, status: 200
+    else
+      render json: @conversation.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def conversation_params
+      params.require(:conversation).permit(:title, :subject, contact_ids: [])
+    end
 end
